c18e75ac50fbe6e2908cbf1d79810bef72ac5d97
Layne <mage@Outlook.com>
2022-03-31 12:36:00 +0800

    second commit

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -9,6 +9,7 @@ public class Demo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
+        System.out.println("111111111");
     }
 
 }
